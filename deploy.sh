034912ba135ac9be383e0559778fb7717255c752
Di Ta <xditaba@gmail.com>
2022-07-11 13:38:52 +0700

    Username of docker is changed from dita to 270319883478

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t dita/multi-client-k8s:latest -t dita/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t dita/multi-server-k8s-pgfix:latest -t dita/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t dita/multi-worker-k8s:latest -t dita/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t 270319883748/multi-client-k8s:latest -t 270319883748/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t 270319883748/multi-server-k8s-pgfix:latest -t 270319883748/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t 270319883748/multi-worker-k8s:latest -t 270319883748/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push dita/multi-client-k8s:latest
-docker push dita/multi-server-k8s-pgfix:latest
-docker push dita/multi-worker-k8s:latest
+docker push 270319883748/multi-client-k8s:latest
+docker push 270319883748/multi-server-k8s-pgfix:latest
+docker push 270319883748/multi-worker-k8s:latest
 
-docker push dita/multi-client-k8s:$SHA
-docker push dita/multi-server-k8s-pgfix:$SHA
-docker push dita/multi-worker-k8s:$SHA
+docker push 270319883748/multi-client-k8s:$SHA
+docker push 270319883748/multi-server-k8s-pgfix:$SHA
+docker push 270319883748/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=dita/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=dita/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=dita/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=270319883748/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=270319883748/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=270319883748/multi-worker-k8s:$SHA
